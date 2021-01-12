65a2ce71a3f6d3340553db389fa175a3bae072a4
siminal <2512509412@qq.com>
2021-01-12 22:43:48 +0800

    java面向对象

---
+++ 面向对象/src/面向对象/StudentTest.java
@@ -0,0 +1,12 @@
+package 面向对象;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+        System.out.println(s2.id);
+        System.out.println(s2.name);
+    }
+}
