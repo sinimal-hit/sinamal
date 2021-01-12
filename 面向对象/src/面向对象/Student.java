855f7f09f7163d93617903a0a4dbec669c40458f
siminal <2512509412@qq.com>
2021-01-12 22:42:55 +0800

    java面向对象

---
+++ 面向对象/src/面向对象/Student.java
@@ -0,0 +1,23 @@
+package 面向对象;
+/*
+    学生类
+        学号  int
+        姓名  String
+    在成员变量来说，没有手动赋值时，系统默认赋值，
+    类型          默认值
+    ------------------
+    byte         0
+    short        0
+    int          0
+    long         0L
+    float        0.0F
+    double       0.0
+    boolean      false
+    char         \u0000
+    引用数据类型    null
+ */
+public class Student {
+    int id;
+    String name;
+
+}
