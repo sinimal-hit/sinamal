0338489a946041dd9074373235f376d1b3030fb0
siminal <2512509412@qq.com>
2021-01-25 18:15:59 +0800

    final

---
+++ 面向对象/src/final关键字/FinalTest01.java
@@ -0,0 +1,11 @@
+package final关键字;
+
+public class FinalTest01 {
+    public static void main(String[] args) {
+        FinalTest01 f1 = new FinalTest01();
+        System.out.println(f1);
+        f1 = new FinalTest01();
+        System.out.println(f1);
+    }
+}
+
