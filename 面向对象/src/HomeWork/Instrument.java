fed032e0840dc3abe5541a6a535f99f3c8b9b6df
siminal <2512509412@qq.com>
2021-02-01 10:20:10 +0800

    Instrument.class,乐器类

---
+++ 面向对象/src/HomeWork/Instrument.java
@@ -0,0 +1,15 @@
+package HomeWork;
+/*
+    作业:
+        定义乐器类Instrument,包括方法:makeSound();
+        定义乐器类的子类:二胡Erhu,钢琴Piano,小提琴Violin
+        定义乐手类,Musician,可以弹奏各种乐器play(Instrument)
+        定义测试类,给不同的的乐器让他弹奏
+*/
+public class Instrument {
+    //定义乐器类
+    public void makeSound(){
+
+    }
+
+}
