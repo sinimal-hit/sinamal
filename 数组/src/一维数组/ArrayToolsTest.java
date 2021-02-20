99b8790e796e471cdd223cd66850bbd96db6f358
siminal <2512509412@qq.com>
2021-02-20 09:51:58 +0800

    java

---
+++ 数组/src/一维数组/ArrayToolsTest.java
@@ -0,0 +1,15 @@
+package 一维数组;
+
+import java.util.Arrays;
+
+/**
+ * SUN公司已经为我们程序员写好了一个数组工具类
+ * java.util.Arrays;
+ *
+ */
+public class ArrayToolsTest {
+    public static void main(String[] args) {
+        int[] arr = {12,31,424,42,2};
+        Arrays.sort(arr);
+    }
+}
