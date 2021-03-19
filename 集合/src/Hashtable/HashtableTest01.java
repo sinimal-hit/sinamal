8eb76bfe2ac3c8d649ff4e37b3b8edd6f410ecb5
siminal <2512509412@qq.com>
2021-03-19 10:43:26 +0800

    3-19

---
+++ 集合/src/Hashtable/HashtableTest01.java
@@ -0,0 +1,14 @@
+package Hashtable;
+/*
+Hashtable的key可以是null吗?
+    Hashtable的key和value都不能为null
+    HashMap集合的key和value都是可以为空的
+Hashtable的方法带有synchronized:线程时安全的
+线程安全有其他方案,Hashtable对线程的处理效率较低,使用较少了.
+
+Hashtable和HashMap一样,底层都是哈希表数据结构,
+Hashtable的初始化容量是11,默认加载因子实际0.75
+
+ */
+public class HashtableTest01 {
+}
