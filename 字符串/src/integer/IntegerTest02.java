181c4ef4fac02a711aff3939f8647c1a14c467e2
siminal <2512509412@qq.com>
2021-03-28 10:16:22 +0800

    3-28

---
+++ 字符串/src/integer/IntegerTest02.java
@@ -0,0 +1,9 @@
+package integer;
+
+public class IntegerTest02 {
+    public static void main(String[] args) {
+        //java9之后不建议使用这个构造方法了,已过时,不必要的装箱
+        Integer i = new Integer(100);
+
+    }
+}
