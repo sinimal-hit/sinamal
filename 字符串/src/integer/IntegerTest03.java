6eefdffa0663e2d76068888ec5142a132bd4973a
siminal <2512509412@qq.com>
2021-03-29 18:52:49 +0800

    3-29

---
+++ 字符串/src/integer/IntegerTest03.java
@@ -0,0 +1,14 @@
+package integer;
+
+/**
+ *  通过访问包扎黄磊的常量,来获取最大值和最小值
+ */
+public class IntegerTest03 {
+    public static void main(String[] args) {
+        System.out.println("int的最大值:"+Integer.MAX_VALUE);//2147483647
+        System.out.println("int的最大值:"+Integer.MIN_VALUE);//-2147483648
+        System.out.println("byte的最大值:"+Byte.MAX_VALUE);//127
+        System.out.println("byte的最小值:"+Byte.MIN_VALUE);//-128
+
+    }
+}
