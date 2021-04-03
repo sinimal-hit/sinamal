e964548c9d17b5287a43c8c63a85116f0baaea62
siminal <2512509412@qq.com>
2021-04-03 14:18:58 +0800

    4-3

---
+++ 字符串/src/string/StringIndexTest.java
@@ -0,0 +1,8 @@
+package string;
+
+public class StringIndexTest {
+    public static void main(String[] args) {
+        String str = "Hello Java, Hello Java String";
+        System.out.println(str.indexOf("Hello",1));
+    }
+}
