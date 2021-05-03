dd502fde8cbed028a821f98c6e5e18b875520c4a
siminal <2512509412@qq.com>
2021-05-03 13:16:56 +0800

    5-3

---
+++ 接口的应用/src/ChinaCooker.java
@@ -0,0 +1,11 @@
+public class ChinaCooker implements FoodMenu{
+    @Override
+    public void shizichaodan() {//中餐厨师做西红柿炒鸡蛋
+        System.out.println("中餐厨师做的西红柿炒鸡蛋");
+    }
+
+    @Override
+    public void yuxiangrusi() {//中餐厨师做鱼香肉丝
+        System.out.println("中餐厨师做鱼香肉丝");
+    }
+}
