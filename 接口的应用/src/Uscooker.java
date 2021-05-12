f52f3baee00b53c60a22b817c20a58f7ebe39021
siminal <2512509412@qq.com>
2021-05-12 14:55:08 +0800

    5-12

---
+++ 接口的应用/src/Uscooker.java
@@ -0,0 +1,11 @@
+public class Uscooker implements FoodMenu{
+    @Override
+    public void shizichaodan() {//西餐厨师做西红柿炒鸡蛋
+        System.out.println("西餐厨师做西红柿炒鸡蛋");
+    }
+
+    @Override
+    public void yuxiangrusi() {//西餐厨师做鱼香肉丝
+        System.out.println("西餐厨师做鱼香肉丝");
+    }
+}
