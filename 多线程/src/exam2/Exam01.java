7e6bbaea82fe723c509bdff405b1daa04eeed466
siminal <2512509412@qq.com>
2021-05-17 17:24:54 +0800

    exam2

---
+++ 多线程/src/exam2/Exam01.java
@@ -0,0 +1,51 @@
+package exam2;
+//面试题:doOther方法执行的时候需要等待doSome方法的结束吗?
+//不需要,因为一个对象共享一把锁
+public class Exam01 {
+    public static void main(String[] args) {
+        MyClass mc1 = new MyClass();
+        MyClass mc2 = new MyClass();
+        Thread t1 = new MyThread(mc1);
+        Thread t2 = new MyThread(mc2);
+        t1.setName("t1");
+        t2.setName("t2");
+        t1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t2.start();
+    }
+}
+
+class MyThread extends Thread{
+    private MyClass mc;
+    public MyThread(MyClass mc){
+        this.mc = mc;
+    }
+    public void run(){
+        if(Thread.currentThread().getName().equals("t1")){
+            mc.doSome();
+        }
+        if(Thread.currentThread().getName().equals("t2")){
+            mc.doOther();
+        }
+    }
+}
+
+class MyClass{
+    public synchronized void doSome(){
+        System.out.println("douSome begin");
+        try {
+            Thread.sleep(1000*10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("douSome over");
+    }
+    public void doOther(){
+        System.out.println("doOther begin");
+        System.out.println("doOther over");
+    }
+}
